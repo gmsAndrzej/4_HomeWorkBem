3b677dabcac5fdefc5f115873af1130bece43cdc
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-05-12 12:34:05 +0200

    Bem conversion

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+console.log("witaj kolego ");
+let button = document.querySelector(".button");
+let types = document.querySelector(".types");
+
+button.addEventListener("click", () => {
+
+    types.classList.toggle("dark");
+
+
+
+
+});
