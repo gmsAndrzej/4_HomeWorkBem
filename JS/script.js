b88f27fa9cb7ef16f40bbcff936c04b3b44c0264
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-05-13 12:00:49 +0200

    finish

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 console.log("witaj kolego ");
-let button = document.querySelector(".button");
+let button = document.querySelector(".body__button");
 let types = document.querySelector(".article__types");
 
 button.addEventListener("click", () => {
