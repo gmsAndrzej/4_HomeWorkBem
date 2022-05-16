ca7ccd6a0f357a20622eb1b1fa9f27a4ce3f2dd2
Andrzej Stoklosa <gmsandrzejstoklosa@gmail.com>
2022-05-16 08:51:02 +0200

    YouCodeCorrect

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,10 @@
 console.log("witaj kolego ");
-let button = document.querySelector(".body__button");
-let types = document.querySelector(".article__types");
+let button = document.querySelector(".js-body__button");
+let types = document.querySelector(".js-article__types");
 
 button.addEventListener("click", () => {
 
     types.classList.toggle("dark");
-
-
-
+   
 
 });
